8a328a628a3328c4b63d35cdbdbfef3af57f3d36
Wenjun_Ma <2046637037@qq.com>
2022-08-23 16:11:45 +0800

    first commit

---
+++ Demonstration on benchmarks/Vorticity_transport_equation_Taylor-Green_Original-GEP.py
@@ -0,0 +1,255 @@
+# %% import modules
+import geppy as gep
+from deap import creator, base, tools
+import numpy as np
+import random
+import operator 
+import pickle
+from fractions import Fraction
+import scipy.io as scio
+import time
+import DHC_GEP as dg
+
+# For reproduction
+s = 0
+random.seed(s)
+np.random.seed(s)
+
+# %% Load data
+
+# Load origin dataset
+train_data = scio.loadmat('../data/Taylor_vortex_flow.mat')
+u = train_data['u']
+v = train_data['v']
+vor = train_data['vor']
+vor_x = train_data['vor_x']
+vor_xx = train_data['vor_xx']
+vor_y = train_data['vor_y']
+vor_yy = train_data['vor_yy']
+u_x = train_data['u_x']
+u_xx = train_data['u_xx']
+u_y = train_data['u_y']
+u_yy = train_data['u_yy']
+v_x = train_data['v_x']
+v_xx = train_data['v_xx']
+v_y = train_data['v_y']
+v_yy = train_data['v_yy']
+Y = train_data['vor_t']      # Target variable is the vor_t.
+
+# Subsample 
+train_point_index = list(np.random.randint(len(Y),size = 720))
+u = u[train_point_index,:]
+v = v[train_point_index,:]
+vor = vor[train_point_index,:]
+vor_x = vor_x[train_point_index,:]
+vor_xx = vor_xx[train_point_index,:]
+vor_y = vor_y[train_point_index,:]
+vor_yy = vor_yy[train_point_index,:]
+u_x = u_x[train_point_index,:]
+u_xx = u_xx[train_point_index,:]
+u_y = u_y[train_point_index,:]
+u_yy = u_yy[train_point_index,:]
+v_x = v_x[train_point_index,:]
+v_xx = v_xx[train_point_index,:]
+v_y = v_y[train_point_index,:]
+v_yy = v_yy[train_point_index,:]
+Y = Y[train_point_index,:]
+viscosity_coef = 15.44 # Diffusion coefficient
+# Y = vor_xx*viscosity_coef+vor_yy*viscosity_coef # If using fully clean data
+
+# %% Creating the primitives set
+# define a protected division to avoid dividing by zero
+def protected_div(x1, x2):
+    if abs(x2) < 1e-30:
+        return 1
+    return x1 / x2
+
+# Define the operators
+pset = gep.PrimitiveSet('Main', input_names=['u','v','vor',
+                                             'vor_x','vor_xx',
+                                             'vor_y','vor_yy',
+                                             'u_x','u_xx',
+                                             'u_y','u_yy',
+                                             'v_x','v_xx',
+                                             'v_y','v_yy'])
+pset.add_symbol_terminal('vis_c', viscosity_coef)
+pset.add_function(operator.add, 2)
+pset.add_function(operator.sub, 2)
+pset.add_function(operator.mul, 2)
+pset.add_function(protected_div, 2)
+pset.add_rnc_terminal() # Add random numerical constants (RNC).
+
+# %% Create the individual and population
+
+# Define the indiviudal class, a subclass of gep.Chromosome
+creator.create("FitnessMin", base.Fitness, weights=(-1,))  # weights=(-1,)/weights=(1,) means to minimize/maximize the objective (fitness).
+creator.create("Individual", gep.Chromosome, fitness=creator.FitnessMin) 
+
+# Register the individual and population creation operations
+h = 5            # head length
+n_genes = 2      # number of genes in a chromosome
+r = 5            # length of the RNC array
+enable_ls = True # whether to apply the linear scaling technique
+
+toolbox = gep.Toolbox()
+toolbox.register('rnc_gen', random.randint, a=-10, b=10)   # each RNC is random integer within [-10, 10]
+toolbox.register('gene_gen', gep.GeneDc, pset=pset, head_length=h, rnc_gen=toolbox.rnc_gen, rnc_array_length=r)
+toolbox.register('individual', creator.Individual, gene_gen=toolbox.gene_gen, n_genes=n_genes, linker=operator.add)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+toolbox.register('compile', gep.compile_, pset=pset)# Compile utility: which translates an individual into an executable function (Lambda)
+
+# %% Define the fitness evaluation function
+
+from numba import jit
+
+# Define the loss for individuals that don't apply the linear scaling technique
+@jit
+def evaluate(individual):
+    """Evalute the fitness of an individual: MAE (mean absolute error)"""
+    func = toolbox.compile(individual)
+    
+    # below call the individual as a function over the inputs
+    Yp = np.array(list(map(func, u,v,vor,
+                                 vor_x,vor_xx,
+                                 vor_y,vor_yy,
+                                 u_x,u_xx,
+                                 u_y,u_yy,
+                                 v_x,v_xx,
+                                 v_y,v_yy))) 
+    
+    # return the MRE
+    return np.mean(abs((Y - Yp)/Y)),
+
+# Define the loss for individuals that apply the linear scaling technique
+# @jit
+def evaluate_ls(individual):
+    """
+    First verify whether the individuals satisfy dimensional homogeneity.
+    If it is not dimensional homogeneous, we would identify it as an invalid individual and directly assign a significant error to it.
+    Otherwise, we would apply linear scaling (ls) to the individual, 
+    and then evaluate its loss: MRE (mean relative error)
+    """
+    func = toolbox.compile(individual)
+    Yp = np.array(list(map(func, u,v,vor,
+                                vor_x,vor_xx,
+                                vor_y,vor_yy,
+                                u_x,u_xx,
+                                u_y,u_yy,
+                                v_x,v_xx,
+                                v_y,v_yy)))
+    if isinstance(Yp, np.ndarray):
+        Q = (np.reshape(Yp, (-1, 1))).astype('float32')
+        Q = np.nan_to_num(Q)
+        individual.a, residuals, _, _ = np.linalg.lstsq(Q, Y)
+
+        # Define the mean relative error (MRE)
+        c = individual.a.reshape(-1,1)
+        relative_error = (np.dot(Q,c)-Y)/Y
+        if residuals.size > 0:
+            return np.mean(abs(relative_error)),
+
+        # Define the relative root mean square error (RRMSE)
+        # residuals is the sum of squared errors
+        # if residuals.size > 0:
+        #     return ((residuals[0])**0.5) / np.linalg.norm(Y, 2),
+    
+    # regarding the above special cases, the optimal linear scaling w.r.t LSM is just the mean of true target values
+    individual.a = 0
+    relative_error = (Y-individual.a)/Y
+    return np.mean(abs(relative_error)),
+
+if enable_ls:
+    toolbox.register('evaluate', evaluate_ls)
+else:
+    toolbox.register('evaluate', evaluate)
+
+# %% Register genetic operators
+toolbox.register('select', tools.selTournament, tournsize=3) # Selection operator
+# 1. general operators
+toolbox.register('mut_uniform', gep.mutate_uniform, pset=pset, ind_pb=0.05, pb=1)
+toolbox.register('mut_invert', gep.invert, pb=0.1)
+toolbox.register('mut_is_transpose', gep.is_transpose, pb=0.1)
+toolbox.register('mut_ris_transpose', gep.ris_transpose, pb=0.1)
+toolbox.register('mut_gene_transpose', gep.gene_transpose, pb=0.1)
+toolbox.register('cx_1p', gep.crossover_one_point, pb=0.3)
+toolbox.register('cx_2p', gep.crossover_two_point, pb=0.2)
+toolbox.register('cx_gene', gep.crossover_gene, pb=0.1)
+# 2. Dc-specific operators
+toolbox.register('mut_dc', gep.mutate_uniform_dc, ind_pb=0.05, pb=1)
+toolbox.register('mut_invert_dc', gep.invert_dc, pb=0.1)
+toolbox.register('mut_transpose_dc', gep.transpose_dc, pb=0.1)
+toolbox.register('mut_rnc_array_dc', gep.mutate_rnc_array_dc, rnc_gen=toolbox.rnc_gen, ind_pb='0.5p')
+toolbox.pbs['mut_rnc_array_dc'] = 1 
+
+# %% Statistics to be inspected
+stats = tools.Statistics(key=lambda ind: ind.fitness.values[0])
+stats.register("avg", np.mean)
+stats.register("std", np.std)
+stats.register("min", np.min)
+stats.register("max", np.max)
+
+# %% Launch evolution
+
+# Define size of population and number of generations
+n_pop = 500             # Number of individuals in a population
+n_gen = 100             # Maximum Generation
+tol = 1e-3               # Threshold to terminate the evolution
+output_type = 'Vorticity_transport_equation_Taylor-Green_Original-GEP'     # Name of the problem
+isRestart = False        # 'True'/'False' for initializing the population with given/random individuals.
+
+# If isRestart is 'True', read the given .pkl file to load the individuals as the first generation population.
+# If isRestart is 'False', initialize the first generation population with random individuals.
+if isRestart:
+    with open("pkl/real-time_Vorticity_transport_equation_Taylor-Green_Original-GEP.pkl",'rb') as file:
+        pop  = pickle.loads(file.read())
+else:
+    pop = toolbox.population(n=n_pop) 
+
+# Only record the best three individuals ever found in all generations
+champs = 3 
+hof = tools.HallOfFame(champs)   
+
+# Evolve
+start_time = time.time()
+pop, log = dg.gep_simple(pop, toolbox, n_generations=n_gen, n_elites=1,
+                          stats=stats, hall_of_fame=hof, verbose=True,tolerance = tol,GEP_type = output_type)
+
+# %% Present our Work and Conclusions
+# print the No.1 best mathematical expression we found:
+
+elapsed = time.time() - start_time
+time_str = '%.2f' % (elapsed)
+
+best_ind = hof[0]
+symplified_best = gep.simplify(best_ind)
+
+if enable_ls:
+    symplified_best = best_ind.a * symplified_best
+
+key= f'When the evolution is terminated, with CPU running {time_str}s, \nOur No.1 best prediction is:'
+with open(f'output/real-time_output_{output_type}.dat', "a") as f:
+    f.write('\n'+ key+ str(symplified_best)+ '\n'+f'with error = {toolbox.evaluate(best_ind)[0]}'+'\n')
+# %% Present our Work and Conclusions
+# print the No.2 best mathematical expression we found:
+if str(gep.simplify(hof[1])) != str(gep.simplify(hof[0])):
+    best_ind = hof[1]
+    symplified_best = gep.simplify(best_ind)
+
+    if enable_ls:
+        symplified_best = best_ind.a * symplified_best
+
+    key= f'Our No.2 best prediction is:'
+    with open(f'output/real-time_output_{output_type}.dat', "a") as f:
+        f.write(key+ str(symplified_best)+ '\n'+f'with error = {toolbox.evaluate(best_ind)[0]}'+'\n')
+# %% Present our Work and Conclusions
+# print the No.3 best mathematical expression we found:
+if str(gep.simplify(hof[2])) != str(gep.simplify(hof[1])) and str(gep.simplify(hof[2])) != str(gep.simplify(hof[0])):
+    best_ind = hof[2]
+    symplified_best = gep.simplify(best_ind)
+
+    if enable_ls:
+        symplified_best = best_ind.a * symplified_best
+
+    key= f'Our No.3 best prediction is:'
+    with open(f'output/real-time_output_{output_type}.dat', "a") as f:
+        f.write( key+ str(symplified_best)+ '\n'+f'with error = {toolbox.evaluate(best_ind)[0]}'+'\n')
---
+++ README.md
@@ -0,0 +1,52 @@
+# Dimensional homogeneity constrained gene expression programming for discovering governing equations from noisy and scarce data
+
+## Abstract
+Data-driven discovery of governing equations is of great significance for helping us to understand physical mechanisms and conduct numerical computations. However, it is still not trivial for the state-of-the-art algorithm to discover the unknown governing equations for complex systems. In this work, a novel dimensional homogeneity constrained gene expression programming (DHC-GEP) method is proposed. DHC-GEP discovers the forms of functions and their corresponding coefficients simultaneously, without assuming any candidate functions in advance. Its advantages including being robust to the hyperparameters of models, the noise level and size of datasets are demonstrated on two benchmarks. Furthermore, DHC-GEP is extended to discover the unknown constitutive relations of two typical non-equilibrium flows. The derived constitutive relations not only are more accurate than the conventional constitutive relations, but also satisfy the Galilean invariance and the second law of thermodynamics. This work provides a general and promising modelling tool, even for fields where large-scale and high-fidelity datasets are intractable to obtain, such as non-equilibrium flows as well as neuroscience, epidemiology, turbulence, and non-Newton fluids.
+
+## Main characteristics of DHC-GEP
+__Fig. a__ is a __schematic diagram of DHC-GEP__. Initial population is created with $N_i$ random individuals. Each individual has genotype (chromosome (CS)) and phenotype (expression tree (ET)). Each chromosome is composed of one or more genes. Via dimensional verification, all the individuals are classified into valid ones and invalid ones according to whether they satisfy dimensional homogeneity. The valid ones would be translated into mathematical expressions (ME), and be evaluated for losses with input data. The invalid ones would be directly assigned a significant loss. Then, the individuals of next generation are generated with the best individual in this generation and the offspring of the selected superior individuals (with relatively lower losses) through genetic operators. The above processes are iteratively conducted until a satisfying individual is obtained. __Schematic diagram of a gene and its corresponding expression tree and mathematical expression are shown in Fig. b__. A gene can be divided into two parts composed of head and tail. The head consists of the symbols from the function set or terminal set, while the tail only consists of symbols from terminal set. Each gene can be exclusively expressed as an expression tree. The domain expressed is called open reading frame (ORF). The expression tree can be further translated into a mathematical expression.
+
+![cesjo](figures/DHC-GEP_Gene.png)
+
+__Strategy of dimensional verification__: first assign prime number tags to the base dimensions and derive the tags for the derived variables, then calculate the dimension of each node in the expression tree from the bottom up, finally compare the tag of the root node with that of the target variable. If they are the same, it can be concluded that the certain individual is dimensional homogeneous.
+
+![cesjo](figures/Dimensional_verification.png)
+
+## Dependencies
+- Python 3.8
+- numpy
+- geppy
+- random
+- operator 
+- pickle
+- fractions
+- scipy
+- time  
+
+[Anoconda](https://www.anaconda.com/) is recommended for installing the above dependencies.
+
+## How to run  our cases
+All the training data are in the 'data' dictionary. 
+
+The scripts are in the corresponding dictionaries. One can run the desired scripts with python.
+
+The best equations of each generation are output in real time to a '.dat' file in the 'Output' dictionary. The latest population is saved every 20 generations to a '.pkl' file in the 'pkl' dictionary for ease of subsequent restarting if necessary. 
+
+## How to assign number tags for the imported terminals
+If someone wants to employ DHC-GEP in other problems, one should reassign number tags for the imported terminals. This is implemented in the following codes.
+```
+# Assign prime number tags to base dimensions
+L,M,T,I,Theta,N,J = 2,3,5,7,11,13,17
+
+# Derive the tags for dirived physical quantities according to their dimensions
+# Note that the tags are always in the form of fractions, instead of floats, which avoids introducing any truncation errors. 
+# Therefore, we use 'Fraction' function here.
+dict_of_dimension = {'rho':Fraction(M,((L)**(3))),
+                     'rho_y':Fraction(M,((L)**(4))),
+                     'rho_yy':Fraction(M,((L)**(5))),
+                     'rho_3y':Fraction(M,((L)**(6))),
+                     'df_c':Fraction((L**2),T)} 
+
+# Assign number tags to taget variable
+target_dimension = Fraction(M,T*((L)**(3)))
+```
